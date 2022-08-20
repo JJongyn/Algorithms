30965e0f7d20a5dae9d81fb2579b7dff6e22f984
jjongyn <jjongyn@gmail.com>
2022-08-20 12:07:25 +0900

    add backjoon 1120

---
+++ 백준/backjoon_1120.py
@@ -0,0 +1,13 @@
+import sys
+
+a, b = sys.stdin.readline().rstrip().split()
+
+result = []
+for i in range(len(b) - len(a) + 1):
+    cnt = 0
+    for j in range(len(a)):
+        if a[j] != b[i+j]:
+            cnt+=1
+    result.append(cnt)
+
+print(min(result))
