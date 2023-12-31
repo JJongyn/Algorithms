06ca3612009740577b66ed3ded75b5d7e48b19a8
jjongyn <jjongyn@gmail.com>
2023-12-31 16:51:46 +0900

    solve problem 1231

---
+++ Programmers/롤케이크자르기(LV2).py
@@ -0,0 +1,19 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/132265
+
+'''
+from collections import Counter
+
+def solution(topping):
+    answer = 0
+    a = Counter(topping) # 형
+    b = set() # 동생
+    
+    for i in topping:
+        a[i] -= 1
+        b.add(i)
+        if a[i] == 0:
+            a.pop(i)
+        if len(a) == len(b):
+            answer += 1
+    return answer
