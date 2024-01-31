2c349084687c28f67dc7a2e847bd7f2328abbf4f
jjongyn <jjongyn@gmail.com>
2024-02-01 00:39:19 +0900

    240131

--- 백준/backjoon_1342.py
+++ 백준/backjoon_1342.py
@@ -5,28 +5,51 @@
 '''
 
 
+# data = input()
+# visited = [False] * len(data)
+# ans = 0
+# results = []
+# def dfs(lst):
+#     global ans
+#     global results
+#     if len(lst) == len(data):
+#         if ''.join(lst) not in results: # 여기서 for문을 또 탐색해야함 .
+#             ans += 1
+#             results.append(''.join(lst))
+#         return
+
+#     for i in range(len(data)):
+#         if len(lst) == 0 or (len(lst) >= 1 and lst[-1] != data[i]): #or (lst[-1] != data[i] and lst[-2] != data[i]):
+#             if not visited[i]:
+                
+#                 lst.append(data[i])
+#                 visited[i] = True
+#                 dfs(lst)
+#                 lst.pop()
+#                 visited[i] = False
+
+# dfs([])
+# print(ans)
+from collections import Counter
+
 data = input()
-visited = [False] * len(data)
-ans = 0
-results = []
-def dfs(lst):
-    global ans
-    global results
-    if len(lst) == len(data):
-        if ''.join(lst) not in results:
-            ans += 1
-            results.append(''.join(lst))
-        return
+counter = Counter(data)
 
-    for i in range(len(data)):
-        if len(lst) == 0 or (len(lst) >= 1 and lst[-1] != data[i]): #or (lst[-1] != data[i] and lst[-2] != data[i]):
-            if not visited[i]:
-                lst.append(data[i])
-                visited[i] = True
-                dfs(lst)
-                lst.pop()
-                visited[i] = False
+def dfs(depth, pre):
+    ans = 0
+    if depth == len(data):
+        return 1
+    
+    for i in counter.keys():
+        if i == pre or counter[i] == 0:
+            continue
+        
+        counter[i] -= 1
+        ans += dfs(depth + 1, i)
+        counter[i] += 1
+    return ans
+print(dfs(0, ''))
+    
+    
 
-dfs([])
-print(ans)
     
