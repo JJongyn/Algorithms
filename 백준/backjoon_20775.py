5e9024ebbdceefd22616f4084133b809eb83f559
jjongyn <jjongyn@gmail.com>
2024-03-04 23:36:32 +0900

    240304

---
+++ 백준/backjoon_20775.py
@@ -0,0 +1,18 @@
+'''
+실버2. https://www.acmicpc.net/problem/2075
+'''
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+data = []
+for _ in range(N):
+    data.append(list(map(int, input().split())))
+
+
+
+
+
+    
